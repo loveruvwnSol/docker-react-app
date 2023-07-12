83f581d70e248686865387074995e666411424d6
sol <ocosio0907@gmail.com>
2023-07-12 14:13:56 +0900

    add data passing between client and server

--- go/src/main.go
+++ go/src/main.go
@@ -1,1 +1,50 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type post struct {
+	ID    int64  `json:"id"`
+	Title string `json:"title"`
+	Text  string `json:"text"`
+}
+
+var posts = []post{{ID: 1, Title: "title1", Text: "hello"}}
+
+func main() {
+	router := gin.Default()
+	router.GET("/post", getPost)
+	router.POST("/post", postText)
+
+	router.Run(":8000")
+	fmt.Println("Starting server at port 8000")
+}
+
+func getPost(c *gin.Context) {
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+	c.IndentedJSON(http.StatusOK, posts)
+}
+
+func postText(c *gin.Context) {
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+	var newPostItem post
+
+	if err := c.BindJSON(&newPostItem); err != nil {
+		return
+	}
+
+	posts = append(posts, newPostItem)
+	c.IndentedJSON(http.StatusCreated, newPostItem)
+}
--- react-app/src/components/organisms/AddPost.tsx
+++ react-app/src/components/organisms/AddPost.tsx
@@ -11,7 +11,13 @@ import {
   Input,
   ModalFooter,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
+
+type Post = {
+  id: number;
+  title: string;
+  text: string;
+};
 
 type AddPostProps = {
   isOpen: boolean;
@@ -19,6 +25,29 @@ type AddPostProps = {
 };
 
 export const AddPost: React.FC<AddPostProps> = ({ isOpen, onClose }) => {
+  const [post, setPost] = useState<Post[]>([]);
+  const [id, setId] = useState<number>(1);
+  const [title, setTitle] = useState<string>("");
+  const [text, setText] = useState<string>("");
+
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    if (title.match(/\S/g) && text.match(/\S/g)) {
+      event.preventDefault();
+      const data = { id, title, text };
+
+      const r = fetch("http://localhost:8000/post", {
+        method: "POST",
+        body: JSON.stringify(data),
+      });
+      setTitle("");
+      setText("");
+      onClose();
+      // await fetchTodo();
+    } else {
+      alert("please input any text");
+    }
+  };
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
@@ -28,12 +57,20 @@ export const AddPost: React.FC<AddPostProps> = ({ isOpen, onClose }) => {
         <ModalBody pb={6}>
           <FormControl>
             <FormLabel>タイトル</FormLabel>
-            <Input placeholder="タイトル" />
+            <Input
+              placeholder="タイトル"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
           </FormControl>
 
           <FormControl mt={4}>
             <FormLabel>つぶやこう</FormLabel>
-            <Input placeholder="つぶやこう" />
+            <Input
+              placeholder="つぶやこう"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
           </FormControl>
         </ModalBody>
 
@@ -43,6 +80,7 @@ export const AddPost: React.FC<AddPostProps> = ({ isOpen, onClose }) => {
             mr={3}
             color={"white"}
             _hover={{ opacity: "0.7" }}
+            onClick={handleSubmit}
           >
             投稿
           </Button>
